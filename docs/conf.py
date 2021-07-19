d0c9bd5f325bed23dbc52690017ef652db15026f
Justin Turner Arthur <justinarthur@gmail.com>
2021-07-19 02:32:15 -0500

    readthedocs compatibility

--- docs/conf.py
+++ docs/conf.py
@@ -22,9 +22,9 @@ author = 'Justin Turner Arthur'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = (
+extensions = [
     'sphinx.ext.autodoc',
-)
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
